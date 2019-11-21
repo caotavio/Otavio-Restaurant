170aec7b98cac3d606456f692af1637c88c5ea88
caotavio <otavio.araujodev@gmail.com>
2019-11-21 21:58:14 +0000

    added search bar and updated to angular 4.3

--- src/app/app.error-handler.ts
+++ src/app/app.error-handler.ts
@@ -1,13 +1,14 @@
-import { Response } from "@angular/http";
+import { HttpErrorResponse } from "@angular/common/http";
 import { Observable } from "rxjs/Observable";
 
 export class ErrorHandler {
     
-    static handleError(error: Response | any) {
+    static handleError(error: HttpErrorResponse | any) {
         let errorMessage: string
 
-        if(error instanceof Response) {
-            errorMessage = `Error ${error.status} when trying to access the URL ${error.url} - ${error.statusText}`
+        if(error instanceof HttpErrorResponse) {
+            const body = error.error
+            errorMessage = `Error ${error.status} when trying to access the URL ${error.url} - ${error.statusText || ''} ${body}`
         } else {
             errorMessage = error.ToString()
         }
