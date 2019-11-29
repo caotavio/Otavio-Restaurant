ef1649353e43267ec846ac255bcc8b2d86a4de5f
caotavio <otavio.araujodev@gmail.com>
2019-11-29 23:28:35 +0000

    added global error handler

--- src/app/app.error-handler.ts
+++ src/app/app.error-handler.ts
@@ -1,19 +1,37 @@
 import { HttpErrorResponse } from "@angular/common/http";
 import { Observable } from "rxjs/Observable";
+import { ErrorHandler, Injectable, Injector, NgZone } from "@angular/core";
+import { NotificationService } from "./shared/messages/notification.service";
+import { LoginService } from "./security/login/login.service";
 
-export class ErrorHandler {
+@Injectable()
+export class ApplicationErrorHandler extends ErrorHandler {
+
+    constructor(private ns: NotificationService,
+                private injector: Injector,
+                private zone: NgZone) {
+        super()
+    }
     
-    static handleError(error: HttpErrorResponse | any) {
-        let errorMessage: string
-
-        if(error instanceof HttpErrorResponse) {
-            const body = error.error
-            errorMessage = `Error ${error.status} when trying to access the URL ${error.url} - ${error.statusText || ''} ${body}`
-        } else {
-            errorMessage = error.ToString()
+    handleError(errorResponse: HttpErrorResponse | any) {
+        if(errorResponse instanceof HttpErrorResponse) {
+            const message = errorResponse.error.message
+            this.zone.run(() => {
+                switch (errorResponse.status) {
+                    case 401:
+                        this.injector.get(LoginService).handleLogin()
+                        break;
+                    case 403:
+                        this.ns.notify(message || 'Not authorized')
+                        break;
+                    case 404:
+                        this.ns.notify(message || 'Not found. Check console for more details')
+                        break;
+                    default:
+                    break;
+                }
+            })
         }
-
-        console.log(errorMessage)
-        return Observable.throw(errorMessage)
+        super.handleError(errorResponse)
     }
 }
