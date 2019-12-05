35f34d3ccbaec1eaca341c9990eb1a455ea9d1dd
caotavio <caotavio@hotmail.com>
2019-12-05 20:21:48 +0000

    heroku deployment config

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/NOME_DO_SEU_PROJETO'));
+//app.use(express.static(__dirname + '/dist/NOME_DO_SEU_PROJETO'));
+app.use(express.static('./public'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/NOME_DO_SEU_PROJETO/index.html'));
+app.get('/*', function(req,res) {   
+    //res.sendFile(path.join(__dirname+'/dist/NOME_DO_SEU_PROJETO/index.html'));
+    res.sendFile(path.join('./public/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
