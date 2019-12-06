ce5f004768a144e7ff7122598eb79496fa728bbf
caotavio <caotavio@hotmail.com>
2019-12-06 18:44:43 +0000

    heroku deployment config

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const distPath = __dirname + '/dist/';
 
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(distPath));
 // app.use(express.static('./'));
 
 app.get('/*', function(req,res) {   
     //res.sendFile(path.join(__dirname+'/dist/NOME_DO_SEU_PROJETO/index.html'));
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(`${distPath}index.html`));
 });
 
 app.listen(process.env.PORT || 8080);
